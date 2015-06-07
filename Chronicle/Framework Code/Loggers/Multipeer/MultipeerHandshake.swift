1cb8c24016d37715a0c0ce870f148963ae8bd8e3
Ben Gottlieb <ben@standalone.com>
2015-06-07 18:41:49 -0500

    added app name and identifier to handshake

--- Chronicle/Framework Code/Loggers/Multipeer/MultipeerHandshake.swift
+++ Chronicle/Framework Code/Loggers/Multipeer/MultipeerHandshake.swift
@@ -19,6 +19,8 @@ public class MultipeerHandshake: NSObject, NSCoding {
 	public var deviceIdentifier: String
 	public var date = NSDate()
 	public var uuid: NSUUID? = NSUUID()
+	public var appIdentifier: String
+	public var appName: String
 	
 	override public init() {
 		#if os(iOS)
@@ -30,23 +32,31 @@ public class MultipeerHandshake: NSObject, NSCoding {
 			deviceType = ""
 			deviceIdentifier = ""
 		#endif
+		
+		appIdentifier = NSBundle.mainBundle().infoDictionary?["CFBundleIdentifier"] as? String ?? ""
+		appName = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as? String ?? ""
+		
 		super.init()
 	}
 	
 	public required init(coder aDecoder: NSCoder) {
-		deviceName = aDecoder.decodeObjectForKey("name") as? String ?? ""
+		deviceName = aDecoder.decodeObjectForKey("dev-name") as? String ?? ""
 		deviceType = aDecoder.decodeObjectForKey("type") as? String ?? ""
-		deviceIdentifier = aDecoder.decodeObjectForKey("id") as? String ?? ""
+		deviceIdentifier = aDecoder.decodeObjectForKey("dev-id") as? String ?? ""
+		appIdentifier = aDecoder.decodeObjectForKey("app-id") as? String ?? ""
+		appName = aDecoder.decodeObjectForKey("app-name") as? String ?? ""
 		date = aDecoder.decodeObjectForKey("date") as? NSDate ?? NSDate()
 		if let string = aDecoder.decodeObjectForKey("uuid") as? String { uuid = NSUUID(UUIDString: string) }
 		super.init()
 	}
 	
 	public func encodeWithCoder(aCoder: NSCoder) {
-		aCoder.encodeObject(self.deviceName, forKey: "name")
+		aCoder.encodeObject(self.deviceName, forKey: "dev-name")
 		aCoder.encodeObject(self.deviceType, forKey: "type")
-		aCoder.encodeObject(self.deviceIdentifier, forKey: "id")
+		aCoder.encodeObject(self.deviceIdentifier, forKey: "dev-id")
 		aCoder.encodeObject(self.date, forKey: "date")
+		aCoder.encodeObject(self.appIdentifier, forKey: "app-id")
+		aCoder.encodeObject(self.appName, forKey: "app-name")
 		if let uuid = self.uuid { aCoder.encodeObject(uuid.UUIDString, forKey: "uuid") }
 	}
 	
